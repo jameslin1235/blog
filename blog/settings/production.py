a1b06a85fd881242a4962470ab457255d9ebd213
jameslin1235 <jameslin1235@gmail.com>
2017-02-25 17:48:57 -0800

    commit #4

--- blog/settings/production.py
+++ blog/settings/production.py
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'deployer',
-        'USER': 'deployerr',
+        'USER': 'deployer',
         'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '',
