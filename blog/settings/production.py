2b9e0dd18e42b05ef0ebcf27ddb0f7aba0c65e09
jameslin1235 <jameslin1235@gmail.com>
2017-02-27 20:24:14 -0800

    commit #11

--- blog/settings/production.py
+++ blog/settings/production.py
@@ -139,9 +139,5 @@ STATICFILES_DIRS = (
 )
 
 
-# Simplified static file serving.
-# https://warehouse.python.org/project/whitenoise/
-
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = '/blog/dashboard/'
